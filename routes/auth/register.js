8af201a957fce05796c069af846c3c879ca55ce8
Drozerah <drozerah@gmail.com>
2020-04-27 18:29:36 +0200

    feat: add ./register + view

---
+++ routes/auth/register.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+
+// middleware that is specific to this router
+// router.use(function timeLog (req, res, next) {
+//   console.log('register call') // !DEBUG
+//   next()
+// })
+/**
+ * Register Route Handle
+ */
+router.route('/')
+  .get((req, res, next) => {
+    try {
+      const config = {
+        css: req.app.locals.css // CSS file
+      }
+      return res.render('auth/register', {
+        config,
+        title: 'Register view'
+      })
+    } catch (error) {
+      console.log('[error][get][/register]') // * error log
+      next(error)
+    }
+  })
+  .post((req, res, next) => {
+    try {
+      return res.send('post register')
+    } catch (error) {
+      console.log('[error][post][/register]') // * error log
+      next(error)
+    }
+  })
+
+module.exports = router

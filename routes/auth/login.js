14518b807ddea228a18307e2e98eef57e614558b
Drozerah <drozerah@gmail.com>
2020-04-27 18:33:26 +0200

    refactor: ./login

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,19 +1,32 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/login', (req, res, next) => {
-  try {
-    const config = {
-      css: req.app.locals.css // CSS file
+/**
+ * Login Route Handler
+ */
+router.route('/')
+  // GET
+  .get((req, res, next) => {
+    try {
+      const config = {
+        css: req.app.locals.css // CSS file
+      }
+      return res.render('auth/login', {
+        config,
+        title: 'Login view'
+      })
+    } catch (error) {
+      console.log('[error][get][/login]') // * error log
+      next(error)
     }
-    return res.render('auth/login', {
-      config,
-      title: 'Login view'
-    })
-  } catch (error) {
-    console.log('[error][get][/login]') // * error log
-    next(error)
-  }
-})
+  })
+  // POST
+  .post((req, res, next) => {
+    try {
+      return res.send('post login')
+    } catch (error) {
+      console.log('[error][post][/login]') // * error log
+      next(error)
+    }
+  })
 
 module.exports = router
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { middleware, visualizer } = require('express-routes-visualizer')
-
+/**
+ * Express Routes Visualier Middleware
+ * @link https://github.com/fdesjardins/express-routes-visualizer
+ */
 router.get('/', middleware({
-  httpMethods: true,
-  routerDir: 'routes' // routes directory
+  httpMethods: true
 }), visualizer({ theme: 'plain' }))
 
 module.exports = router
